b0ddc017945db9fd8fd463bbbf45945e23d32a83
Alexander Skelton <alexanderskelton1997@gmail.com>
2025-11-02 02:13:57 -0700

    fire
    fix

--- src/firebase.js
+++ src/firebase.js
@@ -4,7 +4,7 @@ import { getAnalytics } from "firebase/analytics";
 const firebaseConfig = {
   apiKey: "AIzaSyBZ_ieLkjpdkW3P1zry6R8tRLYjBpH3Pu4",
   authDomain: "skel-co-industries-website.firebaseapp.com",
-  projectId: "skel-co-industries-website",
+  projectId: "127980125819",
   storageBucket: "skel-co-industries-website.firebasestorage.app",
   messagingSenderId: "127980125819",
   appId: "1:127980125819:web:2bd5664207ee95c3c4acde",

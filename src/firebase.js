295027638a2ec1558645cec24798263f9360dc58
Alexander Skelton <alexanderskelton1997@gmail.com>
2025-11-01 21:20:39 -0700

    Initial commit: add gtag

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBZ_ieLkjpdkW3P1zry6R8tRLYjBpH3Pu4",
   authDomain: "skel-co-industries-website.firebaseapp.com",
@@ -16,6 +11,7 @@ const firebaseConfig = {
   measurementId: "G-RY543S2G0H"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+export { app, analytics };
